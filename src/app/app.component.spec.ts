7c946896579a9a0a15ce2bd99b752199e92e0df9
Yanis <mechtayanis20@gmail.com>
2025-03-24 15:05:24 +0100

    Update du design et commencement de la mise en place de la lecture d'un e-book

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ebook-reader' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ebook-reader');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/bibliotheque/bibliotheque.component.ts
+++ src/app/bibliotheque/bibliotheque.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
-import {MatList, MatListItem} from '@angular/material/list';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
+import * as pdfjsLib from 'pdfjs-dist';
+import ePub from 'epubjs';
 
 @Component({
   selector: 'app-bibliotheque',
   imports: [
-    MatIcon
+    MatIcon,
   ],
   templateUrl: './bibliotheque.component.html',
   styleUrl: './bibliotheque.component.css'
 })
 export class BibliothequeComponent {
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
 
+  downloadBook(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  onEBookSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files?.length) {
+      const file = input.files[0];
+      console.log(file);
+    }
+  }
 }
